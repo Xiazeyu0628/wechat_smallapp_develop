94ca7f5595738fbf9c89160f15697d2cde194314
sidisnottheseed <46119879+sidisnottheseed@users.noreply.github.com>
2019-07-25 19:05:21 +0800

    添加了图片说明

---
+++ 说明图片/小程序.mm
@@ -0,0 +1,51 @@
+<map>
+		<node ID="root" TEXT="小程序">		<node TEXT="style（主要是微信官方的UI库）" ID="ab16c270e3fb6101" STYLE="bubble" POSITION="right">
+		</node>
+		<node TEXT="utils（api）" ID="12016c270e933412" STYLE="bubble" POSITION="right">
+		</node>
+		<node TEXT="component（组件—有图形，有逻辑）" ID="10116c26e496a311f" STYLE="bubble" POSITION="right">
+		<node TEXT="calendar组件" ID="1f216c26f475c2034" STYLE="fork">
+		</node>
+		<node TEXT="subpages组件" ID="ca16c26fd97c9165" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="template（模板—有图形，没有逻辑）" ID="12b16c270091a517b" STYLE="bubble" POSITION="right">
+		<node TEXT="ariticles模板" ID="10c16c27013c2d09c" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="data（本地假数据，已弃用）" ID="10216c26f4c0b207d" STYLE="bubble" POSITION="right">
+		</node>
+		<node TEXT="images（小图片）" ID="37a16c26f504500cd" STYLE="bubble" POSITION="right">
+		</node>
+		<node TEXT="pages" ID="8216c26f527ea0bd" STYLE="bubble" POSITION="left">
+		<node TEXT="index（首页）" ID="21b16c26f58f4f107" STYLE="fork">
+		<node TEXT="service（个人信息）" ID="3c516c26f7a677033" STYLE="fork">
+		</node>
+		<node TEXT="timetable（时间表）" ID="29a16c26f7aefb0c3" STYLE="fork">
+		</node>
+		<node TEXT="information（学习资料）" ID="21116c26f7eaed13a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="trends（动态页）" ID="12716c26f596200ea" STYLE="fork">
+		</node>
+		<node TEXT="home（登陆信息页）" ID="2ab16c26f60e83034" STYLE="fork">
+		</node>
+		<node TEXT="details" ID="23116c270290fb04" STYLE="fork">
+		<node TEXT="framework（文章详情页）" ID="23416c2704773910e" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="upload（上传页面）" ID="1b316c2705aec110e" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="app.js" ID="11816c270ec875197" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="app.json" ID="27616c270efba30d7" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="app.wxss" ID="616c270f1658172" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="project.config.json" ID="3de16c270f22400c2" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="sitemap.json" ID="24e16c270f440109c" STYLE="bubble" POSITION="left">
+		</node>
+</node>
+</map>
